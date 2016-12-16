f717c9c90b1d50a9e160ee445d869c5ea8353744
wissam1234 <wissam.ghozlan@gmail.com>
2016-12-16 12:59:42 +0100

    First version

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+/*scroll down*/ 
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top 
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+//scroll to top button
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
+});    
